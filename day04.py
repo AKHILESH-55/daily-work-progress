b32230b9fc876f69ab13cb434c204abe0117b9f9
AKHILESH-55 <akhileshbhatt843800@gmail.com>
2025-12-26 18:20:38 +0530

    Day 04: practice file added

---
+++ day04.py
@@ -0,0 +1,37 @@
+Q-)learn about type casting, booleans, operator list,tuple,dic and their operations and practice questions.
+Ans-)Today i learn about  casting,  booleans, operator list,tuple,dic and their operations and practice questions
+1-)Convert the string "123" into an integer and add 10 to it.
+
+2-)Convert 45.67 into an integer. What happens to the decimal part?
+
+3-)Take an integer x = 100 and convert it into a string. Print its type.
+
+4-)Convert the string "True" into a boolean. What is the output?
+
+5-)Convert 0, 1, -5, and 10 into boolean values.
+
+6-)Check whether a number is positive using boolean expression.
+
+7-)Check if a list [1, 2, 3] is empty or not.
+
+8-)Compare two variables a = 10, b = 20 and store the result in a boolean variable.
+
+9-)Write a program to check if a number is even using boolean logic.
+
+10-)What is the difference between / and //?
+
+11-)Add an element at the end of the list using append()
+
+12-)Remove an element using remove() and pop().
+
+13-)Use slicing to get first 3 elements.
+
+14-)Convert a tuple into a list and modify it.
+
+15-)Check if an element exists in a tuple.
+
+16-)Remove a key using pop().
+
+17-)Get all keys of a dictionary.
+
+
