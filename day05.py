f4a36cb1510d08c1658cb029e83574acacb07016
AKHILESH-55 <akhileshbhatt843800@gmail.com>
2025-12-31 09:49:13 +0530

    Day05:practice file added

---
+++ day05.py
@@ -0,0 +1,19 @@
+Today I learn conditional statements like if elif and practice
+Q)Write a program to check whether a number is positive or negative.
+ 
+Q)Write a program to check whether a number is even or odd.
+ 
+Q)Write a program to check whether a person is eligible to vote (age ≥ 18).
+Write a program to check whether a number is divisible by 5 and 11.
+Q)Write a program to check whether a temperature is:
+ 
+Cold (< 15)
+ 
+Normal (15–30)
+ 
+Hot (> 30)
+ 
+Q)Write a program to check whether a number is a multiple of both 2 and 3.
+Q)Write a program to check whether a year is a leap year.
+ 
+Q)Write a program to check whether a character is a vowel or consonant.
