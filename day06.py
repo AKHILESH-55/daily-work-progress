bb7fd0848d29526e9dfbe9f19f892e78072037d0
AKHILESH-55 <akhileshbhatt843800@gmail.com>
2025-12-31 10:47:17 +0530

    Day06: practice file added

---
+++ day06.py
@@ -0,0 +1,19 @@
+Q)Learn about for loop and while loop and practice coding questions.
+
+(i)Print all prime numbers between 1 and 100 using a for loop.
+
+ (ii)rint the Fibonacci series up to n terms using a for loop.
+
+(iii)Find the largest number in a list using a for loop.
+
+(iv) Count the frequency of each element in a list using a for loop.
+
+(v)Create a pattern using a for loop.
+
+(vi)Write a program that stops when the user enters 0 (use break in while loop).
+
+(vii) Create a number guessing game using a while loop.
+
+(viii) Print the Fibonacci series up to n terms using a while loop.
+
+(viiii) Create a menu-driven program using a while loop.
