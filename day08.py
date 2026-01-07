5d756f0865a31606b54bd7923dd3a073f8fb7e17
AKHILESH-55 <akhileshbhatt843800@gmail.com>
2026-01-07 17:50:56 +0530

    Day 08: practice file added

---
+++ day08.py
@@ -0,0 +1,24 @@
+Q-)learn about *args and **kwargs and practice questions for this topic.
+
+1-)Write a function that accepts any number of integers using *args and returns the largest number among them.
+
+2-)Create a function and find maximum number using *args.
+
+3-)Create a function that takes *args and returns a list of only even numbers.
+
+4-)Create a function that takes *args and returns the total length of all strings passed to it.
+
+5-)Create a function that takes numbers using *args and calculates the average.
+If no values are passed, print "No values given"
+
+6-)Create a function that takes **kwargs and prints all key–value pairs.
+
+7-)Create a function that takes **kwargs and prints all key–value pairs.
+
+8-)Create a function that takes **kwargs and prints all keys in uppercase.
+
+9-)Create a function that checks **kwargs:
+
+If the key name exists → print "Welcome <name>"
+
+If it does not exist → print "Welcome Guest"
