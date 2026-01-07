fb46876d949ffe680d2b2134a8c8b790e7a15069
AKHILESH-55 <akhileshbhatt843800@gmail.com>
2026-01-07 17:31:35 +0530

    Day 07: practice file added

---
+++ day07.py
@@ -0,0 +1,21 @@
+Q-)learn about function and practice questions.
+
+1-)Write a function that takes one number and returns its square.
+
+2-)Write a function to find the maximum of two numbers.
+
+3-)Write a function that returns the average of three numbers.
+
+4-)Write a function that takes a string and returns its length.
+
+5-)Write a function that takes a list and returns the sum of all elements.
+
+6-)Write a function that returns the factorial of a number.
+
+7-)Write a function to check whether a number is positive, negative, or zero.
+
+8-)Write a function that checks if a given year is a leap year.
+
+9-)Write a function that checks whether a string is a palindrome.
+
+10-)Write a function to check whether a number is even or odd.
